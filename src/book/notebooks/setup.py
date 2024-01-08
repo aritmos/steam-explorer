a2522863e05a324426f24d9ee820d6d225ddac12
aritmos <52433836+aritmos@users.noreply.github.com>
2024-01-08 18:30:18 +0100

    Add non-display glue line magic
    This will be used to remove the duplication of cell output in the notebooks. An appreciated change for users inspecting the source code of the website in the repo.

--- src/book/notebooks/setup.py
+++ src/book/notebooks/setup.py
@@ -75,6 +75,31 @@ def glue(line: str) -> None:
         # single variable assignment
         var_name = target.id
         var_value = get_ipython().user_ns[var_name]
+        glue_(var_name, var_value, False)
+        
+    elif isinstance(target, ast.Tuple) or isinstance(target, ast.List):
+        # destructured variable assignment
+        var_names = [element.id for element in target.elts]
+        var_values = [get_ipython().user_ns[var_name] for var_name in var_names]
+
+        for var_name, var_value in zip(var_names, var_values):
+            glue_(var_name, var_value, False)
+
+@register_line_magic
+def glued(line: str) -> None:
+    # execute the line (so the variable can be used later)
+    exec(line, get_ipython().user_ns)
+
+    # glue the assigned variables
+    line_ast = ast.parse(line, mode="exec")
+    target = line_ast.body[0].targets[0]
+
+    
+    
+    if isinstance(target, ast.Name):
+        # single variable assignment
+        var_name = target.id
+        var_value = get_ipython().user_ns[var_name]
         glue_(var_name, var_value, True)
         
     elif isinstance(target, ast.Tuple) or isinstance(target, ast.List):
@@ -86,10 +111,12 @@ def glue(line: str) -> None:
             glue_(var_name, var_value, True)
 
 get_ipython().register_magic_function(glue, magic_kind='line')
+get_ipython().register_magic_function(glued, magic_kind='line')
 
 # --- Plotting ---
 
-font_path = '/usr/share/fonts/HelveticaNeue.ttf'  # Replace with the actual path
+# https://stackoverflow.com/questions/26085867/matplotlib-font-not-found
+# font_path = '/usr/share/fonts/HelveticaNeue/HelveticaNeue.ttf'  # Replace with the actual path
 plt.rcParams['font.family'] = 'Helvetica Neue'
 
 blue = "#7AB"
